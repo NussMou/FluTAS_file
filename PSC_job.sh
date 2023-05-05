bbdf4207fdbd553da2e557be4b3af4079669ec91
Nuss <mmh.nuss@gmail.com>
2023-05-06 02:05:07 +0800

    job5

--- PSC_job.sh
+++ PSC_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=tmp
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=72
+#SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=205g
 #SBATCH --time=01:00:00
@@ -12,7 +12,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/opt/lib
 export PATH=$PATH:$ROOT/opt/bin
 export I_MPI_F90=ifort
 module purge
-module load intel/2021.7.0
-module load intelmpi/2021.7.1
+module load intel/2021.3.0  
+module load intelmpi/2021.3.0-intel2021.3.0
 cd <now_path>
-time mpirun -np 288 <ROOT>/FluTAS/src/flutas.two_phase_ht
+time mpirun -np 512 /home/hpc/b154dc/b154dc22/FluTAS/FluTAS/src/flutas.two_phase_ht
