70948350e09ddf757a6b581fd9dd74e97e0d4ff2
Nuss <mmh.nuss@gmail.com>
2023-05-05 21:49:45 +0800

    job2

--- job.sh
+++ job.sh
@@ -3,12 +3,13 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=72
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=200g
+#SBATCH --mem=205g
 #SBATCH --time=01:00:00
 #SBATCH --output=%j.out
 
 
-source /home/hpc/b154dc/b154dc22/.bashrc
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/opt/lib
+export PATH=$PATH:$ROOT/opt/bin
 export I_MPI_F90=ifort
 module purge
 module load intel/2021.7.0
